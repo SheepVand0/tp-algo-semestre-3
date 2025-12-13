d30400df2abeda1d1fd7aea424ef69b5d386544b
PabloL13 <leolepoutre138@gmail.com>
2025-12-13 14:49:21 +0100

    Ajout des fichiers hashmap.c et hashmap.h

---
+++ application/src/resolution/hashmap.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include "settings.h"
+
+typedef struct HashEntry HashEntry;
+struct HashEntry
+{
+    HashEntry* next;
+    char* key;
+    void* value;
+};
+
+typedef struct HashMap
+{
+    int size;
+    int capacity;
+    HashEntry** entries;
+} HashMap;
+
+/// @brief Crée une table de hachage vide.
+/// 
+/// @param capacity la capacité de la table.
+/// @return La table de hachage créée.
+HashMap* HashMap_New(int capacity);
+
+/// @brief Détruit une table de hachage.
+/// Les clés sont automatiquement libérées mais les données associées ne le sont pas.
+/// L'utilisateur doit donc parcourir le dictionnaire pour les libérer avant d'appeler cette méthode.
+/// 
+/// @param self la table de hachage.
+void HashMap_destroy(HashMap* self);
+
+/// @brief Renvoie la taille d'une table de hachage.
+/// 
+/// @param self la table de hachage.
+/// @return La taille de la table de hachage.
+int HashMap_GetSize(HashMap* self);
+
+/// @brief Renvoie la valeur associée à une clé dans la table de hachage
+/// ou NULL si la clé n'est pas présente.
+/// 
+/// @param self la table de hachage.
+/// @param key la clé.
+/// @return la valeur associée à la clé.
+void* HashMap_Get(HashMap* self, char* key);
+
+/// @brief Ajoute un couple clé/valeur dans une table si la clé n'est pas présente,
+/// sinon modifie la valeur associée à une clé.
+/// La chaîne de caractères représentant la clé est copiée dans la table.
+/// 
+/// @param self la table de hachage.
+/// @param key la clé.
+/// @param value la valeur associée à la clé.
+/// @return Si la clé était déjà présente dans la table, renvoie la valeur précédente
+/// (pour que l'utilisateur puisse libérer la mémoire), sinon renvoie NULL.
+void* HashMap_Insert(HashMap* self, char* key, void* value);
+
+/// @brief Supprime une clé dans une table de hachage.
+/// Cette fonction est sans effet si la clé n'est pas présente.
+/// 
+/// @param self la table de hachage.
+/// @param key la clé.
+/// @return Si la clé est présente dans la table, renvoie la valeur associée
+/// (pour que l'utilisateur puisse libérer la mémoire), sinon renvoie NULL.
+void* HashMap_Remove(HashMap* self, char* key);
